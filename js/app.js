3678f6284cfa61cdcbf0c74d29e27f0c73117722
amibiplob <muhammadbiplob7@gmail.com>
2022-09-04 08:16:26 +0600

    title added

--- js/app.js
+++ js/app.js
@@ -12,9 +12,9 @@ let displayCategory = categories => {
         <li class="nav-item m-2">
         <a onclick="loadCard('${category.category_id}')" class="nav-link bg-secondary bg-opacity-25 rounded-3" href="#">${category.category_name}</a>
         </li>
-
             `
         liCategory.appendChild(li);
+        document.getElementById('spiner').classList.add('d-none')
     }
 }
 
@@ -24,14 +24,16 @@ let loadCard = (id) => {
         .then(data => displayCard(data.data));
 }
 let displayCard = newsCard => {
-   // console.log(newsCard.length)
+    // console.log(newsCard.length)
     document.getElementById("item").innerText = newsCard.length;
+
     let card = document.getElementById('card');
     card.textContent = '';
 
+    document.getElementById('spiner').classList.remove('d-none')
     for (const news of newsCard) {
         //   console.log(news)
-     let div = document.createElement('div');
+        let div = document.createElement('div');
         div.innerHTML = `
         <div class="card border-0 mb-4 shadow bg-secondary bg-opacity-25 rounded-4">
         <div class="row g-0">
@@ -45,17 +47,17 @@ let displayCard = newsCard => {
             <div>
                 <h5 class="card-title">${news.title}</h5>
                 <p class="card-text">${news.details.length > 700 ? news.details.slice(0,
-                    700) + '....' : news.details}</p>
+            700) + '....' : news.details}</p>
             </div>
                     <div class="d-flex justify-content-between align-items-center">
                         <div>
                             <img src="${news.author.img}"
                                 alt="" style="height: 3rem;width: 3rem;" class="rounded-circle">
-                                <h6 class="d-inline">${news.author.name}</h6>
+                                <h6 class="d-inline">${news.author.name ? news.author.name : 'No Data Found'}</h6>
                         </div>
                         <div>
                             <h5>
-                              view : ${news.total_view}
+                              view : ${news.total_view ? news.total_view : 'No Data Found'}
                             </h5>
                         </div>
                         <div class="me-3">
@@ -68,6 +70,7 @@ let displayCard = newsCard => {
     </div>
         `
         card.appendChild(div)
+        document.getElementById('spiner').classList.add('d-none')
     }
 
 }
@@ -80,16 +83,18 @@ let loadModal = (newsId) => {
         .then(data => displayModal(data.data[0]));
 }
 let displayModal = (data) => {
-   let title = document.getElementById('newsModalLabel')
-   title.innerText =`
+    let title = document.getElementById('newsModalLabel')
+    title.innerText = `
    ${data.title}
    `
-   let modalBody = document.getElementById('modal-body')
-   modalBody.innerHTML =`
+    let modalBody = document.getElementById('modal-body')
+    modalBody.innerHTML = `
    <p>Author Name : ${data.author.name ? data.author.name : 'No Data Found'}</p>
    <p>Published Date : ${data.author.published_date ? data.author.published_date : 'No Data Found'}</p>
    <p>Details : ${data.details}</p>
   
    `
 }
+
+
 loadCategory();
